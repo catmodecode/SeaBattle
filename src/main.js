bdb66fd8d53734c73a45c112dc487bd5d42f97ae
catmodecode <catmodecode@gmail.com>
2020-07-25 21:54:05 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from "socket.io-client"
+
+Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketIO('http://127.0.0.1:5000')
+  // vuex: {
+  //     store,
+  //     actionPrefix: 'SOCKET_',
+  //     mutationPrefix: 'SOCKET_'
+  // },
+  // options: { path: "/my-app/" } //Optional options
+}))
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
