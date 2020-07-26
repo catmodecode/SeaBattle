27e3cef235f490ba36539a9bf54c1a213a64686f
Prepad <prepadobnii@gmail.com>
2020-07-27 00:20:07 +0700

    rooms test

--- classes/User.py
+++ classes/User.py
@@ -2,9 +2,10 @@ class User:
     def __init__(self, sid, name):
         self.sid = sid
         self.name = name
+        self.playRoom = None
     
     def setName(self, name):
         self.name = name
-        
+
     def get(self):
         return({'name':self.name,'sid':self.sid})
--- programm.py
+++ programm.py
@@ -2,6 +2,8 @@ import eventlet
 import socketio 
 from classes.User import User
 from classes.UserManager import UserManager
+from classes.Room import Room
+from classes.RoomManager import RoomManager
  
 sio = socketio.Server(cors_allowed_origins='*')
 app = socketio.WSGIApp(sio, static_files={ 
@@ -9,10 +11,11 @@ app = socketio.WSGIApp(sio, static_files={
 }) 
  
 authUsers = UserManager()
+playRooms = RoomManager()
 
 @sio.event 
 def connect(sid, environ): 
-    print('connect ', sid) 
+    print('connect ', sid)
  
 @sio.event
 def auth(sid, name):
@@ -21,13 +24,28 @@ def auth(sid, name):
     else:
         authUsers.userList[authUsers.getBySid(sid)].setName(name)
 
+@sio.event
+def quickroom(sid, data):
+    playRooms.listReadyRoom()
+    if playRooms.readyRoom != []:
+        playRooms.readyRoom[0].setUserTwo(authUsers.userList[authUsers.getBySid(sid)])
+        room = playRooms.readyRoom[0]
+    else:
+        room = Room(authUsers.userList[authUsers.getBySid(sid)],'public')
+        playRooms.addRoom(room,authUsers.userList[authUsers.getBySid(sid)])
+    return room.link
+
 @sio.event 
 def my_message(sid, data): 
     print('message ', data)
  
 @sio.event 
 def disconnect(sid): 
-    authUsers.removeUser(sid)
+    userIndex = authUsers.getBySid(sid)
+    if userIndex != None:
+        if authUsers.userList[userIndex].playRoom != None:
+            playRooms.deleteRoom(authUsers.userList[userIndex].playRoom)
+        authUsers.removeUser(sid)
     print('disconnect ', sid) 
  
 if __name__ == '__main__': 
