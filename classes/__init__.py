5417cf08c53c6a41f89c234bfe419544dd9d762c
Prepad <prepadobnii@gmail.com>
2020-08-04 22:13:47 +0700

    field edit

--- classes/__init__.py
+++ classes/__init__.py
@@ -2,3 +2,6 @@ from classes.User import User
 from classes.UserManager import UserManager
 from classes.Room import Room
 from classes.RoomManager import RoomManager
+from classes.Ship import Ship
+from classes.Cell import Cell
+from classes.Field import Field
